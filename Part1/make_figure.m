a459ec60d5c3a72393e81eeeb386ccef22929536
Michelle <appel.michelle@gmail.com>
2018-04-08 17:10:26 +0200

    figure method updated

--- Part1/make_figure.m
+++ Part1/make_figure.m
@@ -1,11 +1,27 @@
-d = load('datasets\vocab_size_400\data_RGB_dsift_airplanes.mat');
+
+class = 'faces';
+color_space = 'RGB';
+sift_method = 'dsift';
+vocab_size = '400';
+
+d = load(strcat('datasets\vocab_size_', vocab_size, '\data_', ...
+    color_space, '_', sift_method, '_', class, '.mat'));
 features = d.features;
 td = tsne(features);
 color = zeros(length(td), 1);
-color(374:end, :) = 5;
+color(375:end, :) = 5;
 color(end, :) = 8;
 
 figure
-scatter(td(:, 1), td(:, 2), 5, color)
-title('T-sne of the features')
+
+s1 = scatter(td(1:375, 1), td(1:375, 2), 5, [0 0.2 0.8]);
+hold on
+s2 = scatter(td(376:end, 1), td(376:end, 2), 5, [0 0.8 0]);
+
+title(strcat({'T-sne of the BoF of '}, class, {' vs. other classes, vocab size: '}, ...
+    vocab_size, {', '}, sift_method, {', '}, color_space, '.'))
+legend(class, 'other')
+[h, ~] = legend('show');
+
+
 % legend('class 1','other classes','Location','southwest')
