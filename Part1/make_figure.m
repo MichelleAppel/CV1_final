31e5062369e8ffeea2c2eec48bb9fb1b6d4d9aed
Michelle <appel.michelle@gmail.com>
2018-04-08 17:20:53 +0200

    figur

--- Part1/make_figure.m
+++ Part1/make_figure.m
@@ -1,8 +1,8 @@
 
-class = 'faces';
-color_space = 'RGB';
+class = 'airplanes';
+color_space = 'norm_rgb';
 sift_method = 'dsift';
-vocab_size = '400';
+vocab_size = '50';
 
 d = load(strcat('datasets\vocab_size_', vocab_size, '\data_', ...
     color_space, '_', sift_method, '_', class, '.mat'));
@@ -23,10 +23,14 @@ s1 = scatter(td(1:boundary, 1), td(1:boundary, 2), 5, [0 0.2 0.8]);
 hold on
 s2 = scatter(td(boundary+1:end, 1), td(boundary+1:end, 2), 5, [0 0.8 0]);
 
+if strcmpt(color_space, 'norm_rgb')
+   color_space = 'nRGB';
+end
+
 title(strcat({'T-sne of the BoF of '}, class, {' vs. other classes, vocab size: '}, ...
     vocab_size, {', '}, sift_method, {', '}, color_space, '.'))
 legend(class, 'other')
-[h, ~] = legend('show');
+[h, ~] = legend('show', 'location', 'southwest');
 
 
 % legend('class 1','other classes','Location','southwest')
