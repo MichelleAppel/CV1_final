fdb26167c02584b88d89a947dc852919fd8366f0
Michelle <appel.michelle@gmail.com>
2018-04-08 17:17:15 +0200

    updated figure making

--- Part1/make_figure.m
+++ Part1/make_figure.m
@@ -19,9 +19,9 @@ elseif strcmp(class, 'cars')
 end
 
 figure
-s1 = scatter(td(1:375, 1), td(1:375, 2), 5, [0 0.2 0.8]);
+s1 = scatter(td(1:boundary, 1), td(1:boundary, 2), 5, [0 0.2 0.8]);
 hold on
-s2 = scatter(td(376:end, 1), td(376:end, 2), 5, [0 0.8 0]);
+s2 = scatter(td(boundary+1:end, 1), td(boundary+1:end, 2), 5, [0 0.8 0]);
 
 title(strcat({'T-sne of the BoF of '}, class, {' vs. other classes, vocab size: '}, ...
     vocab_size, {', '}, sift_method, {', '}, color_space, '.'))
