6c8b944d4284a72e5e86439d5253c8d2df8a6ab5
Nils Hulzebosch <macnilsmac@hotmail.com>
2018-04-08 21:16:24 +0200

    run final models

---
+++ Part1/run_all.m
@@ -0,0 +1,43 @@
+
+%%%%%%%%%%%%%% DSIFT 1000 %%%%%%%%%%%%%
+
+disp('Start - airplanes - RGB - dsift - 1000');
+main('airplanes', 'RGB', 'dsift', 1000);
+disp('Finish - airplanes - RGB - dsift - 1000');
+
+disp('Start - cars - RGB - dsift - 1000');
+main('cars', 'RGB', 'dsift', 1000);
+disp('Finish - cars - RGB - dsift - 1000');
+
+disp('Start - faces - RGB - dsift - 1000');
+main('faces', 'RGB', 'dsift', 1000);
+disp('Finish - faces - RGB - dsift - 1000');
+
+disp('Start - motorbikes - RGB - dsift - 1000');
+main('motorbikes', 'RGB', 'dsift', 1000);
+disp('Finish - motorbikes - RGB - dsift - 1000');
+
+
+disp('Start - airplanes - norm_rgb - dsift - 1000');
+main('airplanes', 'norm_rgb', 'dsift', 1000);
+disp('Finish - airplanes - norm_rgb - dsift - 1000');
+
+disp('Start - cars - norm_rgb - dsift - 1000');
+main('cars', 'norm_rgb', 'dsift', 1000);
+disp('Finish - cars - norm_rgb - dsift - 1000');
+
+disp('Start - faces - norm_rgb - dsift - 1000');
+main('faces', 'norm_rgb', 'dsift', 1000);
+disp('Finish - faces - norm_rgb - dsift - 1000');
+
+disp('Start - motorbikes - norm_rgb - dsift - 1000');
+main('motorbikes', 'norm_rgb', 'dsift', 1000);
+disp('Finish - motorbikes - norm_rgb - dsift - 1000');
+
+
+disp('Start - airplanes - opoponent - dsift - 200');
+main('airplanes', 'opponent', 'dsift', 200);
+disp('Finish - airplanes - opoponent - dsift - 200');
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
